bcf03533bc567dc08f41a48aefd6a872a97327fd
MauricioReis <mauricioreis.tads@gmail.com>
2021-10-21 22:52:18 -0300

    Primeira vercao

---
+++ main.js
@@ -0,0 +1,33 @@
+const linksSocialMedia = {
+  github: 'mauricioPReis',
+  youtube: 'UCyVXwPqCE58M2nackM9mcsw',
+  facebook: 'akanomreiss',
+  instagram: 'orion_mauricio',
+  twitter: 'MauricioPaivaR2'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://www.${social}.com/${linksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLinkGithub.href = data.url
+      userPhoto.src = data.avatar_url
+      userNickName.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
